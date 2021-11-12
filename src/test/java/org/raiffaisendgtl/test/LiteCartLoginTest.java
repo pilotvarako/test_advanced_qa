c98eb68b28f5c5e903b2d6b259a1160f56abec4b
Vladislav Varako <pilotvarako@gmail.com>
2021-11-13 00:45:19 +0600

    Добавлен сценарий логина

---
+++ src/test/java/org/raiffaisendgtl/test/LiteCartLoginTest.java
@@ -0,0 +1,36 @@
+package org.raiffaisendgtl.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LiteCartLoginTest {
+
+    private static ChromeDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\programming\\other\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void test() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+}
